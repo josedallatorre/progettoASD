0a666e86cea3b4a09c361fb1f78c6f33a5617ab2
josedallatorre <jose.dallatorre@hotmai.com>
2021-03-01 18:28:37 +0100

    PeriodNaive is working

--- PeriodNaive.c
+++ PeriodNaive.c
@@ -11,16 +11,12 @@ int main(int argc, char const *argv[])
 {
     char a[MAX_LINE_SIZE];
     int size = scanArray(a); 
-<<<<<<< HEAD
     int p = PeriodNaive(a, size);
     printf("%d", p);
-=======
->>>>>>> 1a9f952f1e5f4942e147010f749e8ca2212683e4
     
     return 0;
 }
 
-<<<<<<< HEAD
 int PeriodNaive(char *s, int n){
     for (int p = 1; p <= n; p++)
     {
@@ -45,12 +41,6 @@ char *substr(char *src, int i, int j){
     return dest;
 }
 
-=======
-int PeriodNaive(char *s, int size){
-    
-}
-
->>>>>>> 1a9f952f1e5f4942e147010f749e8ca2212683e4
 int scanArray(char *a) {
     // scan line of text
     char line[MAX_LINE_SIZE];
