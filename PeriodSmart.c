df8e33266961ddfbe2fcfc55f6e3261e08e82c91
josedallatorre <jose.dallatorre@hotmai.com>
2021-03-02 18:34:57 +0100

    scritto pseudocodice, da sistemare

--- PeriodSmart.c
+++ PeriodSmart.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "PeriodNaive.h"
+#include "PeriodSmart.h"
 #include <malloc.h>
 
 #define MAX_LINE_SIZE 1000   // maximum size of a line of input
@@ -18,7 +18,24 @@ int main(int argc, char const *argv[])
 }
 
 int PeriodSmart(char *s, int n){
-    return;
+    int r[n-1];
+    r[0]=0;
+    for (int i = 1; i <= n-1; i++)//per calcolare r[i+1]
+    {
+        int z=r[i];
+        while ((z>0)&&(s[i+1]!=s[z+1]))
+        {
+            z=r[z];
+        }
+        if (s[i+1]==s[z+1])
+        {
+            r[i+1]= z+1;
+        }else{
+            r[i+1]=0;
+        }
+        return n - r[n];        
+    }
+    
 }
 
 
---
+++ PeriodSmart.h
@@ -0,0 +1,5 @@
+//scan line from input and convert it to an array
+int scanArray(char *);
+
+//compute the period 
+int PeriodSmart(char *, int);
