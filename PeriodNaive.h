bba6bcb8e72f0c0c3f6b12c4fd967409184d3c40
josedallatorre <jose.dallatorre@hotmai.com>
2021-04-01 15:17:34 +0200

    everything seems work

--- PeriodNaive.h
+++ PeriodNaive.h
@@ -1,6 +1,24 @@
+//number of letters used i.e. 3 = 'a','b','c'
+#define NUM_LETT 2
+//
+#define NMAX 500000
+#define NMIN 1000
+
+
 //scan line from input and convert it to an array
 int scanArray(char *);
 
 //compute the period 
 int PeriodNaive(char *, int);
 
+//compute the resolution
+long getResolution();
+
+//get the letter from alphabet
+char getLetter();
+
+//return a string from alphabet with given length
+char *getString(int);
+
+//return difference between two given time
+long getDiffTime(struct timespec end, struct timespec start);
--- PeriodSmart.c
+++ PeriodSmart.c
@@ -2,25 +2,80 @@
 #include <stdlib.h>
 #include <string.h>
 #include "Period.h"
-
-
+#include <time.h>
+#include <math.h>
+#include <unistd.h>
+#include <fcntl.h>
 #define MAX_LINE_SIZE 1000   // maximum size of a line of input
-
+#define CLOCK_MONOTONIC 1
 int main(int argc, char const *argv[])
-{
-    char a[MAX_LINE_SIZE];
-    int size = scanArray(a); 
-    char *b = (char *)malloc(size+1*sizeof(char));
-    //metto i valori da b[1..n]
-    memcpy(b+1,a, size);
-    int p = PeriodSmart(b, size);
-    printf("%d", p);
-    free(b);
+{   
+    long R = getResolution();
+    int A = NMIN;
+    double B = exp((log(NMAX)-log(A))/99);
+    long Tmin= R*(1/0.001+1);
+    struct timespec start, end;  
+    for (int j = 0; j < 100; j++)
+    {
+        printf("inizio la %d-esima iterazione\n",j);
+        int iter =0;
+        int nj = A * pow(B,j);
+        char *s = getString(nj);
+        long t;
+        clock_gettime(CLOCK_MONOTONIC,&start);
+        do{ 
+            PeriodSmart(s,nj);    
+            iter++;
+            clock_gettime(CLOCK_MONOTONIC,&end);
+            t = getDiffTime(end, start);
+            printf("Tmin: %ld time: %ld k: %d j: %d\n",Tmin,t, iter,j);
+        }while(t<Tmin);
+        long exec_time = t/iter;
+        FILE *fd = fopen("timesSmart.txt","a");
+        fprintf(fd, "n:%d time:%ld\n",nj,exec_time);
+        fclose(fd);
+        free(s);
+    }
+  
+    
     return 0;
 }
 
+long getDiffTime(struct timespec end, struct timespec start){
+    long temp = (((end.tv_sec - start.tv_sec)* 1.e+9 ) + 
+    ((end.tv_nsec - start.tv_nsec)));
+    return temp;
+}
+char getLetter(){
+    int i = 97 + (rand()%NUM_LETT);  //uso la tabella ASCII
+    char c = i;    //converto da num ascii a char
+    return c;
+}
+char *getString(int n){
+    char *s = (char *)malloc(n+1*sizeof(char));
+    for (int i = 0; i <= n; i++)
+    {
+        if(i==0){            //uso i valori compresi tra
+                             //s[1..n]  
+        }else{
+            char c = getLetter();
+            s[i] = c;
+        }
+    }
+    return s;
+}
+
+    
+long getResolution() {                        // fare attenzione alle divisioni su valori di tipo intero... 
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    do {
+        clock_gettime(CLOCK_MONOTONIC, &end);
+    } while(start.tv_nsec == end.tv_nsec);
+    return (end.tv_nsec - start.tv_nsec);
+}
 int PeriodSmart(char *s, int n){
-    int *r = (int*)malloc((n+1)* sizeof (int) );
+    int r [n+1];
     r[1]=0;
     int z=0;
     for (int i = 1; i <= n-1; i++)//per calcolare r[i+1]
@@ -39,7 +94,6 @@ int PeriodSmart(char *s, int n){
 
     }
     int risp = n - r[n];
-    free(r);
     return risp; 
     }
 
