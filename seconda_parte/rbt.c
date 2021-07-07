ddef6c9cd3a16d72230ee0d0068a64a4e55c2ea2
josedallatorre <jose.dallatorre@hotmai.com>
2021-07-07 18:50:38 +0200

    rbt not working, syntax error

---
+++ seconda_parte/rbt.c
@@ -0,0 +1,177 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "rbt.h"
+int main(int argc, char const *argv[])
+{   
+    //initialize the root
+    Node *root = createNode(0, "NULL");
+    //handleInput(root);
+    freeTree(root);
+    return 0;
+}
+Node *rbtInsert(Node *root, Node *node){
+    Node *y = NULL;
+    Node *x = root;
+    while(x != NULL){
+	y = x;
+        if(node->key < x->key){
+	    x = x->left;
+	}else{
+	    x = x->right;
+	}
+    node->parent = y;
+    if(y == NULL){
+	root = node;
+    }
+    else if(node->key < y->key){
+	y->left = node;
+    }
+    else{
+	y->right = node;
+    }
+    node->left = NULL;
+    node->right = NULL;
+    node->color = red;
+    rbtInsertFixup(root, node);
+    return root;
+    }
+}
+void rbtInsertFixup(Node *root, Node *node){
+    while(node->parent->color == red){
+	if(node->parent == node->parent->parent->left){
+	    node *y = NULL;
+	    node *y = node->parent->parent->right;
+	    if(y->color == red){ //case 1 
+		node->parent->color = black;
+		y->color = black;
+		node->parent->parent->color = red;
+		node = node->parent->parent;
+	    }
+	    else if(node = node->parent->right){//case 2
+		node = node->parent;
+		leftRotate(root, node);
+		}
+	    node->parent->color = black;//case 3
+	    node->parent->parent->color = red;
+	    rightRotate(root, node->parent->parent);
+	}
+	else{
+	    node *y = node->parent->parent->left;
+	    if(y.color == red){ //case 1 
+		node->parent->color = black;
+		y->color = black;
+		node->parent->parent->color = red;
+		node = node->parent->parent;
+	    }
+	    else if(node ==node->parent->left){//case 2
+		node = node->parent;
+		rightRotate(root, node);
+		}
+	    node->parent->color = black;//case 3
+	    node->parent->parent->color = red;
+	    leftRotate(root, node->parent->parent);
+	}
+	root->color = black;
+    }
+}
+
+void leftRotate(Node *root, Node *x){
+    Node *y = x->right;
+    x->right = y-> left;//turn y subtree into x right subtree
+    if (y->left != NULL){
+	y->left->parent = x;
+    }
+    y->parent = x->parent;
+    if(x->parent == NULL){
+	root = y;
+    }
+    else if(x == x->parent->left){
+	x->parent->left = y;
+    }
+    else{
+	x->parent->right = y;
+    }	
+    y->left = x;
+    x->parent = y;
+}
+void rightRotate(Node *root, Node *x){
+    Node *y = x->left;
+    x->left = y->right; //turn y subtree into x left subtree
+    if(y->right !=NULL){
+	y->right->parent = x;
+    }
+    y->parent = x->parent
+    if(x->parent ==NULL){
+	root = y;
+    }
+    else if(x == x->parent->right){
+	x->parent->right = y;
+    }
+    else{
+	x->parent->left = y;
+    }
+    y->right = x;
+    x->parent = y;
+}
+
+
+void handleInput(Node *root){
+    //initialize variable for the input
+    char function[20];
+    int key;
+    char text[30];
+    scanf("%s", function);
+    while (strcmp(function, "exit")!= 0)
+    {
+        if ((strcmp(function, "insert"))==0)
+        {
+            scanf("%d %s",&key, text);
+            Node *node = createNode(key, text);
+            root = rbtInsert(root, node);
+        }
+        if ((strcmp(function, "show"))==0)
+        {
+            show(root);
+        }
+         if ((strcmp(function, "find"))==0)
+        {
+            printf("TODO find\n");
+        }
+        scanf("%s", function);
+    }
+}
+void show(Node *node){
+    if (node == NULL)
+    {
+        printf("NULL ");
+    }else{
+        printf("%d:%s:%d ", node->key, node->text, node->height);
+        show(node->left);
+        show(node->right);
+    }
+    
+}
+Node *createNode(int key, char *text){
+    struct Node *node = malloc(sizeof(struct Node));
+    node->key = key;
+    node->text = text;
+    node->height = 0;
+    node->color = red;
+    node->left= NULL;
+    node->right= NULL;
+    node->parent = NULL;
+    return node;
+}
+void freeTree(Node *node){
+    if (node == NULL)
+    {
+        return;
+    }else{
+        Node *right = node->right;
+        Node *left = node->left;
+        free(node);
+        freeTree(right);
+        freeTree(left);
+    }
+}
---
+++ seconda_parte/rbt.h
@@ -0,0 +1,25 @@
+typedef enum {black, red} COLOR;
+typedef struct Node
+{
+    int key;
+    char *text;
+    int height;
+    COLOR color;
+    struct Node *left, *right, *parent;
+}Node;
+//handle input, given root(pass to other function)
+void handleInput(Node *);
+//show the avl
+void show(Node *);
+//create a new node, given key, text
+Node *createNode(int, char *);
+//free space
+void freeTree(Node *);
+//insert a node in AVL
+Node *rbtInsert(Node *, Node*);
+//restore rbt properties
+void rbtInsertFixup(Node *, Node*);
+//compute a left rotation of a given node
+void leftRotate(Node *, Node *);
+//compute a right rotation of a given node
+void rightRotate(Node *, Node *);
