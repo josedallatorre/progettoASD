3d2c41f9b7968f7967061e729f91e20e03e5bfc6
josedallatorre <jose.dallatorre@hotmail.com>
2021-07-21 19:11:45 +0200

    everything works

---
+++ seconda_parte/times.h
@@ -0,0 +1,7 @@
+#define CLOCK_MONOTONIC 1
+#define NMAX 1000000
+#define NMIN 1000
+//compute the resolution
+extern long getResolution();
+//return difference between two given time
+extern long getDiffTime(struct timespec end, struct timespec start);
